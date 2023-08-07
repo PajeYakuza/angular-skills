835cd706403d0dbfc378e4cbbad53db593599246
Paje <pedroaugusto@iuptec.com.br>
2023-08-07 03:32:08 -0300

    Teste Angular Skills em funcionamento

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  cards: Array<any>;
+  public loading: boolean = true;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.httpClient.get('/api/skills').subscribe((ret: Array<any>) => {
+      this.cards = ret;
+      this.loading = false;
+    });
+  }
+
+}
